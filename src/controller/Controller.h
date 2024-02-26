ad11aa9da5830c744074cf198c452cba5cbd4f22
kev4cards <21962903+kev4cards@users.noreply.github.com>
2024-02-25 22:34:32 -0500

    Update Controller.h

--- src/controller/Controller.h
+++ src/controller/Controller.h
@@ -11,7 +11,8 @@
 
 #define EXTENDED_SCANCODE_BIT (1 << 8)
 #define AXIS_SCANCODE_BIT (1 << 9)
-#define MAX_AXIS_RANGE 85.0f
+#define MAX_AXIS_RANGE 85.0
+#define MAX_DIAGONAL 69.0
 
 namespace LUS {
 enum GyroData { DRIFT_X, DRIFT_Y, GYRO_SENSITIVITY };
@@ -25,9 +26,10 @@ struct DeviceProfile {
     int32_t Version = 0;
     bool UseRumble = false;
     bool UseGyro = false;
+    bool UseStickDeadzoneForButtons = false;
     float RumbleStrength = 1.0f;
     int32_t NotchProximityThreshold = 0;
-    std::unordered_map<int32_t, float> AxisDeadzones;
+    std::unordered_map<int32_t, double> AxisDeadzones;
     std::unordered_map<int32_t, float> AxisMinimumPress;
     std::unordered_map<int32_t, float> GyroData;
     std::map<int32_t, int32_t> Mappings;
@@ -54,10 +56,10 @@ class Controller {
     void SetButtonMapping(int32_t portIndex, int32_t deviceButtonId, int32_t n64bitmask);
 
     std::shared_ptr<DeviceProfile> GetProfile(int32_t portIndex);
-    int8_t& GetLeftStickX(int32_t portIndex);
-    int8_t& GetLeftStickY(int32_t portIndex);
-    int8_t& GetRightStickX(int32_t portIndex);
-    int8_t& GetRightStickY(int32_t portIndex);
+    double& GetLeftStickX(int32_t portIndex);
+    double& GetLeftStickY(int32_t portIndex);
+    double& GetRightStickX(int32_t portIndex);
+    double& GetRightStickY(int32_t portIndex);
     int32_t& GetPressedButtons(int32_t portIndex);
     float& GetGyroX(int32_t portIndex);
     float& GetGyroY(int32_t portIndex);
@@ -72,17 +74,17 @@ class Controller {
     int32_t mDeviceIndex;
     std::string mControllerName = "Unknown";
 
-    int8_t ReadStick(int32_t portIndex, Stick stick, Axis axis);
-    void ProcessStick(int8_t& x, int8_t& y, float deadzoneX, float deadzoneY, int32_t notchProxmityThreshold);
+    double ReadStick(int32_t portIndex, Stick stick, Axis axis);
+    void ProcessStick(double& x, double& y, double deadzoneX, double deadzoneY, int32_t notchProxmityThreshold);
     double GetClosestNotch(double angle, double approximationThreshold);
 
   private:
     struct Buttons {
         int32_t PressedButtons = 0;
-        int8_t LeftStickX = 0;
-        int8_t LeftStickY = 0;
-        int8_t RightStickX = 0;
-        int8_t RightStickY = 0;
+        double LeftStickX = 0.0;
+        double LeftStickY = 0.0;
+        double RightStickX = 0.0;
+        double RightStickY = 0.0;
         float GyroX = 0.0f;
         float GyroY = 0.0f;
     };
